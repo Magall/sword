0d2d4afcb7d40d7fa0649dc9e4c0814c63a968c6
magall <rafitogm2@gmail.com>
2023-04-18 17:54:58 -0300

    [chore]Config and base files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {useAuthStore} from "./store/auth";
+import {onMounted} from "vue";
+
+const store = useAuthStore();
+
+onMounted(() => {
+  store.autoLogin();
+})
+
+</script>
+
+<template>
+  <div>
+    <router-view/>
+  </div>
+</template>
+
+
---
+++ src/views/discovery/Discovery.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Discovery</h1>
+</template>
